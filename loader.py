afc2a56b5f6906746cf9ca54a7a2ce12efa060ea
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-06 23:50:15 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -15,26 +15,30 @@ os.chdir(default_directory)
 list_int = os.listdir(path=".")
 text_length_int = len(os.listdir(path="."))
 os.chdir(default_directory)
-for i in range (text_length_int):
-    if (list_int[i] == "intel"):
-        os.chdir("intel")
-        path_int = os.getcwd()
-list_int_1 = os.listdir(path=".")
-text_length_int_1 = len(os.listdir(path="."))
 
-for i in range (text_length_int_1):
-    if (list_int_1[i] == model):
-        os.chdir(model)
-        path_fac = os.getcwd()
-list_fac = os.listdir(path=".")
-text_length_fac = len(os.listdir(path="."))
-for i in range (text_length_fac):
-    if (list_fac[i] == "FP16"):
-        os.chdir("FP16")
-        path_fac_FP = os.getcwd()
-list_fac_mod = os.listdir(path=".")
-text_length_fac_mod = len(os.listdir(path="."))
-shutil.move(model+'.bin',model_directory)
-shutil.move(model+'.xml',model_directory)
-os.chdir(default_directory)
-shutil.rmtree('intel')
+try:
+    for i in range (text_length_int):
+        if (list_int[i] == "intel"):
+            os.chdir("intel")
+            path_int = os.getcwd()
+    list_int_1 = os.listdir(path=".")
+    text_length_int_1 = len(os.listdir(path="."))
+
+    for i in range (text_length_int_1):
+        if (list_int_1[i] == model):
+            os.chdir(model)
+            path_fac = os.getcwd()
+    list_fac = os.listdir(path=".")
+    text_length_fac = len(os.listdir(path="."))
+    for i in range (text_length_fac):
+        if (list_fac[i] == "FP16"):
+            os.chdir("FP16")
+            path_fac_FP = os.getcwd()
+    list_fac_mod = os.listdir(path=".")
+    text_length_fac_mod = len(os.listdir(path="."))
+    shutil.move(model+'.bin',model_directory)
+    shutil.move(model+'.xml',model_directory)
+    os.chdir(default_directory)
+    shutil.rmtree('intel')
+except Exception:
+    pass
